6ec02daeb26aafca27774c49aa0f5a50c4a3eec1
IumoInfinium <iumoinfinium1331@gmail.com>
2023-01-21 08:34:07 +0530

    babble working

---
+++ public_chat/wsgi.py
@@ -0,0 +1,19 @@
+"""
+WSGI config for public_chat project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+#import pymysql
+
+#pymysql.install_as_MySQLdb()
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'public_chat.settings')
+
+application = get_wsgi_application()
